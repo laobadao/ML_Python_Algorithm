c9a2bdd03a4276fe84514244eee8fd90748ccb04
2296616900@qq.com <2296616900@qq.com>
2017-09-08 14:06:20 +0800

    basic learning

---
+++ hello.py
@@ -0,0 +1,22 @@
+print ("Hello,Python! and Hello Python")
+
+if True :
+    print("oo true")
+else :
+    print("oo false")
+
+print("111","number","string")
+
+print("100*2+33-90*2/2==",100*2+33-90*2/2)
+
+print("请输入你的名字:")
+name= input()
+
+print("hello",name,"nice to meet you")
+
+name =input("please calculate 1024*768=")
+
+print("well,the true answer is =",1024*768,"  did you right?")
+
+#
+# name
