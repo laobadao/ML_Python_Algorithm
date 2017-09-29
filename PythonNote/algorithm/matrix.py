c61763be48c669cbdd8d48d230c15fcd13738440
2296616900@qq.com <2296616900@qq.com>
2017-09-29 14:06:31 +0800

    rectangles

--- PythonNote/algorithm/matrix.py
+++ PythonNote/algorithm/matrix.py
@@ -32,9 +32,9 @@ M = array([[0, 0, 1, 1, 0, 0, 0, 0, 1],
            [0, 0, 1, 1, 1, 1, 1, 1, 1],
            [1, 0, 0, 0, 1, 0, 1, 0, 0], ])
 
-left_up_points = []   # 左上 list
-left_down_points = []   # 左下 list
-right_up_points = []    # 右上 list
+left_up_points = []  # 左上 list
+left_down_points = []  # 左下 list
+right_up_points = []  # 右上 list
 right_down_points = []  # 右下 list
 
 # 矩阵的行
@@ -47,7 +47,7 @@ n = M.shape[1]
 # 行数从上往下 0 —— m-1 列数从左往右 0 —— n-1
 for i in range(m - 1):
     for j in range(n - 1):
-        if M[i, j] == M[i, j+1] == M[i+1, j] == 1:
+        if M[i, j] == M[i, j + 1] == M[i + 1, j] == 1:
             left_up_points.append((i, j))
 
 # 循环遍历寻找右上角，也就是这个点和这个点的下方以及左边一点都是 1 ，然后加到 right_up_points list 中
@@ -55,42 +55,55 @@ for i in range(m - 1):
 #  注意： 当 矩阵满足条件是，右上角 的坐标 查询最快的话 也是 （0,1） 也就是 列数 纵坐标肯定大于1
 for i in range(m - 1):
     for j in range(1, n):
-        if M[i, j] == M[i, j-1] == M[i + 1, j] == 1:
+        if M[i, j] == M[i, j - 1] == M[i + 1, j] == 1:
             right_up_points.append((i, j))
 
 # 循环遍历左下角，也就是这个点的右边以及上边 一点是1 ，。。。
 # 但这个点不可能是（0，n -1）( m-1 ,n -1)
 for i in range(1, m):
     for j in range(n - 1):
-        if M[i, j] == M[i-1, j] == M[i, j+1] == 1:
+        if M[i, j] == M[i - 1, j] == M[i, j + 1] == 1:
             left_down_points.append((i, j))
 
 # 循环遍历右下角 ，也就是这个点以及点的上方和左边都是 1
 
 for i in range(1, m):
     for j in range(1, n):
-        if M[i, j] == M[i-1, j] == M[i, j-1] == 1:
+        if M[i, j] == M[i - 1, j] == M[i, j - 1] == 1:
             right_down_points.append((i, j))
 
+# 找出所有可能的长方形 4 重 for 循环遍历，左上 x == 右上 x ， 右上 y==右下 y ，左 上y = 左下 y ,右下x= 左下 x
+# (left_up_x,left_up_y) ( left_down_x,left_down_y) (right_up_x ,right_up_y) (right_down_x,right_down_y)
+# left_up_x == right_up_x & right_up_y == right_down_y & right_down_x == left_down_x & left_up_y & left_down_y
+
+rectangles = []
+for left_up_point in left_up_points:
+    for right_up_point in right_up_points:
+        for left_down_point in left_down_points:
+            for right_down_point in right_down_points:
+                if left_up_point[0] == right_up_point[0]:
+                    if right_up_point[1] == right_down_point[1]:
+                        if left_up_point[1] == left_down_point[1]:
+                            if right_down_point[0] == left_down_point[0]:
+                                if (left_up_point != right_up_point) & (right_up_point != right_down_point) \
+                                        & (right_down_point != left_down_point) & (left_down_point != left_up_point) \
+                                        & (left_up_point != right_down_point) & (right_up_point != left_down_point):
+                                    rectangles.append(
+                                        (left_up_point, right_up_point, left_down_point, right_down_point))
+
+
+
+
+
+
+
+
 # print(M)
+
 # print(left_up_points)
+# print(left_down_points)
+# print(right_up_points)
+# print(right_down_points)
 # print(list(range(3)))  # 不包含 最后一个 小于3
 # print(list(range(1, 3)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print(rectangles)
