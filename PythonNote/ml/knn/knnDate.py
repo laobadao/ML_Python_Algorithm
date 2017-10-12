f69d28d39d17b707ba626148d4d72abb514e1e49
2296616900@qq.com <2296616900@qq.com>
2017-10-12 16:50:29 +0800

    test classify algorithm

--- PythonNote/ml/knn/knnDate.py
+++ PythonNote/ml/knn/knnDate.py
@@ -4,6 +4,74 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import matplotlib.lines as mlines
+import operator
+
+"""
+函数说明：KNN 算法，分类器
+
+:parameter
+    testX - 用于分类的数据 （测试集）
+    trainSet - 用于训练的数据 （训练集）
+    labels - 用于分类的标签
+    k - kNN 算法参数，选择距离最小的 k 个点
+
+:return
+    sortedClassCount[0][0] 分类结果
+
+:date
+    2017-09-29
+"""
+
+"""
+
+note：
+    补充 numpy 函数 
+    1. 创建二维数组
+    2. 求数组行数
+    3. 求数组列数 
+"""
+
+
+def classify0(testX, trainSet, labels, k):
+    # numpy函数 shape[0] 返回 trainSet 的行数
+    trainSetSize = trainSet.shape[0]
+    # 在列向量方向上重复 testX 共1次(横向)，行向量方向上重复 testX 共dataSetSize次(纵向)
+    #  np.tile(testX, (trainSetSize, 1)) 是为了 将测试数据  test = [101, 20] 构造成 和 训练集相同的数据形式
+    #  这样可以使用 欧式距离公式  多个维度之间的距离公式
+    # 在欧几里得空间中，点x = (x1, ..., xn)和 y = (y1, ..., yn)之间的欧氏距离为
+    diffMat = np.tile(testX, (trainSetSize, 1)) - trainSet
+    # 二维特征相减后平方
+    sqDiffMat = diffMat ** 2
+    # sum()所有元素相加，sum(0)列相加，sum(1)行相加 axis＝0表示按列相加，axis＝1表示按照行的方向相加
+    sqDistances = sqDiffMat.sum(axis=1)
+    # 开方，计算出距离
+    distances = sqDistances ** 0.5
+    # distances: [ 128.68954892  118.22436297   16.55294536   18.43908891]
+    # 返回distances中元素从小到大排序后的索引值  因为函数返回的排序后元素在原array中的下标
+    # 上面四个数 在array中的下标 索引是 128.68954892 - 0  ,118.22436297- 1 ,16.55294536 - 2 ,18.43908891- 3
+    # 排序后是 16.55294536  18.43908891 118.22436297 128.68954892  所以 索引 下标的排序就是 2 3 1 0
+    sortedDistIndices = distances.argsort()
+    # print("sortedDistIndices", sortedDistIndices)
+    # sortedDistIndices [2 3 1 0]
+    # 定一个记录类别次数的字典  字典 类似于 map 存储 键值对
+    classCount = {}
+    for i in range(k):
+        # 取出前k个元素的类别
+        voteIlabel = labels[sortedDistIndices[i]]
+        # dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
+        # 计算类别次数  比如 第一次 加入 武侠片时 先取之前存过的 武侠片的值 没有则默认 0 classCount.get(voteIlabel, 0)
+        # 也就是 2 对应的 武侠片 先存到 classCount 字典中 然后 累加次数
+        # 爱情片 同理 第一次 字典中没有 因为是空的 或者 只有武侠 或只有爱情 然后默认0 再加1
+        # 循环到 i =1 取出 索引 3 的值 对应的 labels 是武侠 再取字典之前的1 累加 2
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    # python3中用items()替换python2中的iteritems()
+    # key=operator.itemgetter(1)根据字典的值进行排序  在这个字典中，爱情片是键，1 是值
+    # key=operator.itemgetter(0)根据字典的键进行排序
+    # reverse降序排序字典  从大到小排序
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    # 返回次数最多的类别,即所要分类的类别  前三个值里面 武侠片出现两次 分类为 武侠片
+    return sortedClassCount[0][0]
+
 
 """
 函数说明:打开并解析文件，对数据进行分类：1代表不喜欢,2代表魅力一般,3代表极具魅力
@@ -82,7 +150,7 @@ def normData(dataSet):
     # [ 0.        0.        0.001156]
     maxVals = dataSet.max(0)
     # [  9.12730000e+04   2.09193490e+01   1.69551700e+00]
-    print("maxVals", maxVals)
+    # print("maxVals", maxVals)
     # 计算最大最小值的区间
     ranges = maxVals - minVals
     # 创建和 dataSet 一样行列数的0 矩阵
@@ -91,6 +159,9 @@ def normData(dataSet):
     m = dataSet.shape[0]
     # 原始矩阵 减去 最小矩阵 公式上半部分
     # print("np.tile(minVals, (m, 1))", np.tile(minVals, (m, 1)))
+    # 需要注意的是，特征值矩阵有1000×3 个值，
+    # 而minVals和range的值都为1×3。为了解决这个问题，我们使用NumPy库中tile()
+    # 函数将变量内容复制成输入矩阵同样大小的矩阵，
     normDataSet = dataSet - np.tile(minVals, (m, 1))
     # print("normDataSet:", normDataSet)
     # 除以公式下半部分 最大值和最小值的差 得到归一化数据
@@ -98,6 +169,49 @@ def normData(dataSet):
     normDataSet = normDataSet / np.tile(ranges, (m, 1))
     # print("normDataSet:", normDataSet)
     return normDataSet, ranges, minVals
+
+
+"""
+函数说明:分类器测试函数
+
+Parameters:
+    无
+Returns:
+    normDataSet - 归一化后的特征矩阵
+    ranges - 数据范围
+    minVals - 数据最小值
+
+Modify:
+    2017-10-12
+"""
+
+
+def datingClassTest():
+    # 打开的文件名
+    fileName1 = "datingTestSet.txt"
+    # 将返回的特征矩阵和分类向量分别存储到datingDataMat和datingLabels中
+    datingDataMat, datingLabels = file2Matrix(fileName1)
+    # 取所有数据的百分之十
+    hoRatio = 0.10
+    # 数据归一化,返回归一化后的矩阵,数据范围,数据最小值
+    normMat, ranges, minVals = normData(datingDataMat)
+    # 获得normMat的行数
+    m = normMat.shape[0]
+    # 百分之十的测试数据的个数 前 m 行数据
+    numTestVecs = int(m * hoRatio)
+    # 分类错误计数
+    errorCount = 0.0
+
+    # 取 10% 的数据然后判断 每一行是一个数据 ，将每一行 作为测试集 放到 所有剩余 90% 的训练集中测试
+    for i in range(numTestVecs):
+        # 前 numTestVecs 100个数据作为测试集,后 m - numTestVecs 900 个数据作为训练集
+        #  numTestVecs:m 是为了取区间用的 所有后面要加上：m --- datingLabels 是list
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 4)
+        print("分类器分类结果:%d\t训练集中真实类别:%d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+            print("第 i %d 个数据分类错了" % i)
+    print("错误率:%f%%" % (errorCount / float(numTestVecs) * 100))
 
 
 """
@@ -181,12 +295,15 @@ Returns:
 
 if __name__ == '__main__':
     # 打开的文件名
-    fileName = "datingTestSet.txt"
-    datingMat, datingLabels = file2Matrix(fileName)
-    normDataSet, ranges, minVals = normData(datingMat)
-    print("normDataSet", normDataSet)
-    print("ranges", ranges)
-    print("minVals", minVals)
+    # fileName = "datingTestSet.txt"
+    # datingMat, datingLabels = file2Matrix(fileName)
+    # normDataSet, ranges, minVals = normData(datingMat)
+    # print("normDataSet", normDataSet)
+    # print("ranges", ranges)
+    # print("minVals", minVals)
+    # 测试分类器算法 KNN
+    datingClassTest()
+
 # print(datingMat)
 # print(datingLabels)
 # showData(datingMat, datingLabels)
