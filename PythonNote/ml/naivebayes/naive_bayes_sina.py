0d65c19cd17cd740356ca167e55693f88413ee31
2296616900@qq.com <2296616900@qq.com>
2017-10-23 16:32:10 +0800

    文本处理

--- PythonNote/ml/naivebayes/naive_bayes_sina.py
+++ PythonNote/ml/naivebayes/naive_bayes_sina.py
@@ -83,10 +83,13 @@ def TextProcessing(folder_path, test_size=0.2):
     train_list = data_class_list[index:]  # 训练集
     # [:index] 从0 开始 到该索引，不包括 该 index 的数据
     test_list = data_class_list[:index]  # 测试集
+    # zip(* ) * 代表解压缩 将之前打乱的 训练数据 和 相应类别的合计，再还原 成 两个独立的list
     train_data_list, train_class_list = zip(*train_list)  # 训练集解压缩
     test_data_list, test_class_list = zip(*test_list)  # 测试集解压缩
 
-    all_words_dict = {}  # 统计训练集词频
+    all_words_dict = {}  # 统计训练集词频 字典 key 词  对应 value 出现的次数
+    # 双层 for 循环 ，给一个 空 字典 添加 key  并复制 value 的基本思路
+    # 不存在 则加入 该KEY 且 初始化 赋值1 ，存在则累计 加1
     for word_list in train_data_list:
         for word in word_list:
             if word in all_words_dict.keys():
@@ -94,14 +97,86 @@ def TextProcessing(folder_path, test_size=0.2):
             else:
                 all_words_dict[word] = 1
 
-    # 根据键的值倒序排序
+    # for word_list in train_data_list:
+    #     for word in word_list:
+    #         if word in all_words_dict.keys():
+    #             all_words_dict[word] += 1
+    #         else:
+    #             all_words_dict[word] = 1
+
+    # 根据键的值倒序排序  也就是 单词出现贫度 越高的 往前排
     all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f: f[1], reverse=True)
     all_words_list, all_words_nums = zip(*all_words_tuple_list)  # 解压缩
     all_words_list = list(all_words_list)  # 转换成列表
     return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
 
 
+"""
+函数说明:读取文件里的内容，并去重
+
+Parameters:
+    words_file - 文件路径
+Returns:
+    words_set - 读取的内容的set集合
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-08-22
+Note:
+    ZJ studied in 2017-10-23
+"""
+
+
+def MakeWordsSet(word_file):
+    # 创建 set 集合 
+    word_set = set()
+    # 打开文件 
+    with open(word_file, 'r', encoding='utf-8') as f:
+        # 一行一行读取
+        for line in f.readlines():
+            word = line.strip()  # 去除首尾空白符
+            if len(line) > 0:  # 若 文件 不为空 len() > 0
+                word_set.add(word)  # 则添加到 set 中 ，set 中是去重的
+    return word_set
+
+
+"""
+函数说明:文本特征选取
+
+Parameters:
+    all_words_list - 训练集所有文本列表
+    deleteN - 删除词频最高的deleteN个词
+    stopwords_set - 指定的结束语
+Returns:
+    feature_words - 特征集
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-08-22
+"""
+
+
+def words_dict(all_words_list, deleteN, stopwords_set=set()):
+    feature_words = []  # 特征列表
+    n = 1
+    for t in range(deleteN, len(all_words_list), 1):
+        if n > 1000:  # feature_words的维度为1000
+            break
+            # 如果这个词不是数字，并且不是指定的结束语，并且单词长度大于1小于5，那么这个词就可以作为特征词
+        if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(
+                all_words_list[t]) < 5:
+            feature_words.append(all_words_list[t])
+        n += 1
+    return feature_words
+
+
 if __name__ == '__main__':
     # 文本预处理
     folder_path = './SogouC/Sample'  # 训练集存放地址
-    TextProcessing(folder_path)
+    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path,
+                                                                                                        test_size=0.2)
+    print(all_words_list)
