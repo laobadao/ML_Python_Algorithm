6cdedaeb7e9b87e958a8090054adb89e150d9416
2296616900@qq.com <2296616900@qq.com>
2017-10-25 16:32:05 +0800

    logistic

--- PythonNote/ml/logistic/logistic_gradient.py
+++ PythonNote/ml/logistic/logistic_gradient.py
@@ -108,6 +108,50 @@ def sigmoid(inX):
 
 
 """
+函数说明:梯度上升算法
+
+Parameters:
+	dataMatIn - 数据集
+	classLabels - 数据标签
+Returns:
+	weights.getA() - 求得的权重数组(最优参数)
+Author:
+	Jack Cui
+Blog:
+	http://blog.csdn.net/c406495762
+Zhihu:
+	https://www.zhihu.com/people/Jack--Cui/
+Modify:
+	2017-08-28
+Note:
+    ZJ studied in 2017-10-25
+"""
+
+
+def gradAscent(dataMatIn, classLabels):
+    # dataMatIn 数据集转换成 numpy 的 mat 矩阵
+    dataMatrix = np.mat(dataMatIn)
+    # 数据标签转化为 numpy 的 mat 矩阵，并进行转置 transpose()
+    labelMat = np.mat(classLabels).transpose()
+    # 计算 dataMatrix 矩阵的大小 ，返回 行数 m ，列数 n
+    m, n = np.shape(dataMatrix)
+    # 每次移动的步长，也就是学习速率，控制更新的幅度
+    alpha = 0.001
+    # 最大迭代次数
+    maxCycles = 500
+    # 创建 n 行1 列的矩阵 数据全是1 ，n 是前面的列数 也就是 3 行 1 列
+    weights = np.ones((n, 1))
+    for k in range(maxCycles):
+        # dataMatrix 相当于X矩阵 weights 相当于 w^T
+        h = sigmoid(dataMatrix * weights)
+        # sigmoid 函数计算出来 返回的数跟实际的类别做对比
+        error = labelMat - h
+        weights = weights + alpha * dataMatrix.transpose() * error
+        # 将矩阵转换为数组 getA() ，返回权重数组
+    return weights.getA()
+
+
+"""
 函数说明:绘制数据集
 
 Parameters:
@@ -167,9 +211,66 @@ def plotDataSet():
     plt.show()
 
 
+"""
+函数说明:绘制数据集
+
+Parameters:
+    weights - 权重参数数组
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Zhihu:
+    https://www.zhihu.com/people/Jack--Cui/
+Modify:
+    2017-08-30
+"""
+
+
+def plotBestFit(weights):
+    dataMat, labelMat = loadDataSet()  # 加载数据集
+    dataArr = np.array(dataMat)  # 转换成numpy的array数组
+    n = np.shape(dataMat)[0]  # 数据个数
+    xcord1 = []
+    ycord1 = []  # 正样本
+    xcord2 = []
+    ycord2 = []  # 负样本
+    for i in range(n):  # 根据数据集标签进行分类
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])  # 1为正样本
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])  # 0为负样本
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  # 添加subplot
+    ax.scatter(xcord1, ycord1, s=20, c='red', alpha=.5)  # 绘制正样本
+    ax.scatter(xcord2, ycord2, s=20, c='green', alpha=.5)  # 绘制负样本
+	# weights 是前面求出来的回归系数 arange(-3.0, 3.0, 0.1) 区间 从-3 到3 间隔 0.1
+    x = np.arange(-3.0, 3.0, 0.1)
+    # ？？？ 干嘛的？
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+
+    plt.title('BestFit')  # 绘制title
+    plt.xlabel('X1')
+    plt.ylabel('X2')  # 绘制label
+    plt.show()
+
+
 if __name__ == '__main__':
     # gradient_ascent_test()
     # 1.999999515279857 也就是 x 为 1.999999515279857 四舍五入2 时 可以取得函数最大值
     # dataMat, labelMat = loadDataSet()
     # print(dataMat)
-    plotDataSet()
+    # plotDataSet()
+
+    dataMat, labelMat = loadDataSet()
+    weights = gradAscent(dataMat, labelMat)
+    plotBestFit(weights)
+# 这是求解出来的回归系数 [w0,w1,w2]
+# [[ 4.12414349]
+# [ 0.48007329]
+# [-0.6168482 ]]
