2a5ccd246f0883e55d71d56fd2e553b5239d3a24
2296616900@qq.com <2296616900@qq.com>
2017-10-26 15:52:00 +0800

    m
    sklearn logistic regression conclusion

--- PythonNote/ml/logistic/logistic_horse.py
+++ PythonNote/ml/logistic/logistic_horse.py
@@ -2,6 +2,7 @@
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 import random
+from scipy.special import expit, logit
 
 """
 函数说明:sigmoid函数
@@ -55,7 +56,7 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
         for i in range(m):
             alpha = 4 / (1.0 + j + i) + 0.01  # 降低alpha的大小，每次减小1/(j+i)。
             randIndex = int(random.uniform(0, len(dataIndex)))  # 随机选取样本
-            h = sigmoid(sum(dataMatrix[randIndex] * weights))  # 选择随机选取的一个样本，计算h
+            h = expit(sum(dataMatrix[randIndex] * weights))  # 选择随机选取的一个样本，计算h
             error = classLabels[randIndex] - h  # 计算误差
             weights = weights + alpha * error * dataMatrix[randIndex]  # 更新回归系数
             del (dataIndex[randIndex])  # 删除已经使用的样本
@@ -89,7 +90,7 @@ def gradAscent(dataMatIn, classLabels):
     maxCycles = 500  # 最大迭代次数
     weights = np.ones((n, 1))
     for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)  # 梯度上升矢量化公式
+        h = expit(dataMatrix * weights)  # 梯度上升矢量化公式
         error = labelMat - h
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights.getA()  # 将矩阵转换为数组，并返回
@@ -211,20 +212,24 @@ Modify:
 
 
 def classifyVector(inX, weights):
-    # θ^T * X
-    prob = sigmoid(sum(inX * weights))
+    # θ^T * X  logistic_function() sigmoid() expit
+    prob = expit(sum(inX * weights))
     if prob > 0.5:
         return 1.0
     else:
         return 0.0
 
 
-if __name__ == '__main__':
+def logistic_function(x):
+    return .5 * (1 + np.tanh(.5 * x))
 
+
+if __name__ == '__main__':
     colicTest()
     # 35.82%
     colicTest1()
     # 28.36%
+
     # 结论：
     # 当数据集较小时，我们使用梯度上升算法
     # 当数据集较大时，我们使用改进的随机梯度上升算法
